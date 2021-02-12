b3bf8c60cc819970a699b031806e779156a03b2a
Andrii-Aloshyn <78973125+Andrii-Aloshyn@users.noreply.github.com>
2021-02-12 20:48:08 +0200

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,23 @@
+
+
+
+
+
+let map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 48.472430, lng: 35.014061 },
+    zoom: 8,
+  });
+}
+
+
+
+
+
+
+
+
+
+
